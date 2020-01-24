89b2afbeb3b0ad3926ee9748a80f7aa249c0c1ab
danieluss <danieluss@pm.me>
2020-01-24 01:24:58 +0100

    small amendment

--- src/main/java/db/train/repository/SpecificationFactory.java
+++ src/main/java/db/train/repository/SpecificationFactory.java
@@ -8,14 +8,13 @@ import java.util.List;
 public class SpecificationFactory {
 
     public static <T> Specification<T> containsTextInAttributes(String text, Class<T> clazz) {
-        text = "%" + text + "%";
-        String finalText = text;
+        String finalText = "%" + text + "%";
         return (root, query, builder) ->
                 builder.or(
                         root.getModel()
                                 .getDeclaredSingularAttributes()
                                 .stream()
-                                .map(a -> builder.like(root.get(a.getName()), finalText))
+                                .map(a -> builder.like(root.get(a.getName()).as(String.class), finalText))
                                 .toArray(Predicate[]::new)
         );
     }
