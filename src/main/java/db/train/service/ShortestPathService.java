fd924711134f9bc476a691f31f0bbef0a2182958
danieluss <danieluss@pm.me>
2020-01-25 16:43:53 +0100

    added connection from station list endpoint

---
+++ src/main/java/db/train/service/ShortestPathService.java
@@ -0,0 +1,46 @@
+package db.train.service;
+
+import db.train.persistence.model.Connection;
+import db.train.persistence.model.Station;
+import db.train.web.dto.StationList;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.webrepogen.annotations.ServicePackage;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class ShortestPathService {
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    private static class Node {
+
+    }
+
+    public Connection connect(StationList stationList) {
+        List<Station> resultingConnection = new ArrayList<>();
+        resultingConnection.add(stationList.getStations().get(0));
+        for (int i = 0; i < stationList.getStations().size() - 1; i++) {
+            resultingConnection
+                    .addAll(
+                            connect(
+                                    stationList.getStations().get(i),
+                                    stationList.getStations().get(i + 1)));
+        }
+        Connection connection = stationList.getData();
+        connection.setStations(resultingConnection.stream().map(Station::getId).collect(Collectors.toList()));
+        return connection;
+    }
+
+
+    public List<Station> connect(Station from, Station to) { // return path (from; to]
+        //TODO
+        return new ArrayList<>();
+    }
+
+}
