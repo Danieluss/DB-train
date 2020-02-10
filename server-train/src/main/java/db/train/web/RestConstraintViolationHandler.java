6a9bf86c8930a8fa222e66263feb5a5a88d1566e
danieluss <danieluss@pm.me>
2020-02-10 22:17:01 +0100

    more generic exception rest logging

--- server-train/src/main/java/db/train/web/RestConstraintViolationHandler.java
+++ server-train/src/main/java/db/train/web/RestConstraintViolationHandler.java
@@ -1,32 +1,25 @@
 package db.train.web;
 
 import org.hibernate.JDBCException;
+import org.hibernate.exception.ConstraintViolationException;
 import org.postgresql.util.PSQLException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.ConstraintViolation;
-
-import org.hibernate.exception.ConstraintViolationException;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestConstraintViolationHandler extends ResponseEntityExceptionHandler {
 
     private List<String> getErrors(PSQLException sqlException) {
         List<String> errors = new ArrayList<>();
@@ -36,8 +29,8 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
         return errors;
     }
 
-    @ExceptionHandler(value = { ConstraintViolationException.class })
-    protected ResponseEntity<Object> handleConflict(ConstraintViolationException ex, WebRequest request) {
+    @ExceptionHandler(value = { JDBCException.class})
+    protected ResponseEntity<Object> handleConflict(JDBCException ex, WebRequest request) {
         StringWriter sw = new StringWriter();
         ex.printStackTrace(new PrintWriter(sw));
         Map<String, List<String>> returnObject = new HashMap<>();
@@ -46,4 +39,5 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
         return handleExceptionInternal(ex, returnObject,
                 new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
+
 }
